f8ed27ae97daac8569590ec342f589394286f3a8
Gunjan Arora <159866503+gun03-hub@users.noreply.github.com>
2024-07-07 18:34:48 +0530

    Day-12.py

---
+++ Day-12.py
@@ -0,0 +1,39 @@
+# P1-LINKED-LIST-CYCLE
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow!= fast:
+            if fast is None or fast.next is None:
+                return False
+
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
+# P2-MERGE-TWO-SORTED-LISTS
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
