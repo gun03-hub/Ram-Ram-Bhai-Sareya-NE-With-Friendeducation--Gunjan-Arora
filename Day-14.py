37ced3e7b5e81ad427c12454aaa373e803ac32de
Gunjan Arora <159866503+gun03-hub@users.noreply.github.com>
2024-07-11 22:02:25 +0530

    Day-14.py

---
+++ Day-14.py
@@ -0,0 +1,31 @@
+#P-1 Maximum Depth of Binary Tree
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            return max(left_depth, right_depth) + 1
+#P-2 Construct Binary Tree from Preorder and Inorder Traversal
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+
+        return root
